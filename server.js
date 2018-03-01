2b9f41ec277884cabcc6292f1f9c69265917109c
rajshah9914 <34474021+rajshah9914@users.noreply.github.com>
2018-03-01 19:02:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,67 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var past={
+           title: 'Past | Raj Shah',
+           date: 'March 1,2018',
+           content:`
+           <p>
+            I have always tried to be at top of my best.
+            My previous status prooves the same.
+            <br>
+            </p>
+            <div>
+            <p>
+            10th result:96.2% ICSE From R.B.K School <br>
+            Admitted myself towards Success Gauranteed Institute:----IIT POINT<br>
+            12th result:92%
+            </p>
+            <p>
+            <iframe width="420" height="345" src="https://www.youtube.com/embed/6q3wAHMJqm4? autoplay=1">
+            </iframe>
+            </p>
+            <p>
+            Be Awaited for next future updates!
+            Thanks for watching.
+            </p>
+            </div>`
+};
+function createtemplate(obj){
+    var title=obj.title;
+    var date=obj.date;
+    var content= obj.content;
+var htmlTemplate= `
+    <html>
+    <head> 
+    <title>${title}</title>
+     <link href="/ui/style.css" rel="stylesheet" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body style="background-color:powderblue">
+    <div class="home">
+        <a href="\">Home</a>
+    </div>
+    <hr>
+    <div class="date">
+        ${date}
+    </div>
+    <div class="past">
+    
+    <div>
+       ${content}
+    </div>
+    </body>    
+    </html>`
+    ;
+    return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/past', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'past.html')); 
+   res.send(createtemplate(past)); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
