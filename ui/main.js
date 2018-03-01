3e948146127d8b9fc5329485d04ab90843ef701b
rajshah9914 <34474021+rajshah9914@users.noreply.github.com>
2018-03-01 20:19:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
 console.log('Loaded!');
 var element=document.getElementById('unique1');
 element.innerhtml='Hey Raj, First Webapp Establishing Success!!!!!';
+var img=document.getElementById('madi');
+function moveRight()
+{
+  var marginLeft=0;
+   img.style.marginLeft= marginLeft +  'px';
+}
+img.onclick=function(){
+   
+     var interval=setInterval(moveRight,100);
+};
