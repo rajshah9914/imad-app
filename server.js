3a8a0f5f57fb51a69742d382ee6c92de016863ae
rajshah9914 <34474021+rajshah9914@users.noreply.github.com>
2018-03-01 16:11:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/past', function (req, res){
+   res.sendFile('My 10th marks are 96.2%'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
